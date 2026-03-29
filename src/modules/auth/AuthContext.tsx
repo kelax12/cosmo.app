6212d9e0ab268dcfbf1de481636b441dd4d4c449
kelax12 <kelaxbusiness@gmail.com>
2026-03-29 16:30:51 +0200

    Update AuthContext.tsx

--- src/modules/auth/AuthContext.tsx
+++ src/modules/auth/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase, isSupabaseConfigured } from '../../lib/supabase';
 import { User as SupabaseUser } from '@supabase/supabase-js';
 
 export type User = {
