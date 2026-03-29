d1a30428be15c2d70ca2560e7c35dab79a84ae67
kelax12 <kelaxbusiness@gmail.com>
2026-03-29 16:17:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ const PageWithSuspense: React.FC<{ children: React.ReactNode }> = ({ children })
 const AppRoutes = () => (
   <Routes>
     {/* Landing page - outside Layout */}
-    <Route path=\"welcome\" element={<PageWithSuspense><LandingPage /></PageWithSuspense>} />
+    <Route path="welcome" element={<PageWithSuspense><LandingPage /></PageWithSuspense>} />
     
     {/* Layout wrapper for all routes */}
     <Route element={<LayoutWithSuspense />}>
